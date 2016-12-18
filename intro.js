dd82ae55be1be5a0756a2af95980f1efdec8e23b
Richard Wang <richardwang@Richards-MacBook-Pro-2.local>
2016-12-18 02:46:10 -0500

    finished fibo

--- intro.js
+++ intro.js
@@ -9,6 +9,9 @@ Add the following to your previous work:
 var list = document.getElementsByTagName("li");
 var thelist = document.getElementById("thelist");
 var btn = document.getElementById("b");
+var fibobtn = document.getElementById("f");
+var fibolist = document.getElementById("fibolist");
+var fibonum = document.getElementsByClassName("fol")
 
 // When the button is pushed, add an element to the list
 var add = function() {
@@ -34,6 +37,26 @@ var rm = function() {
     this.remove();
 };
 
+// To generate the next fibonacci number
+var fibo = function(e) {
+    var first = 1;
+    var second = 1;
+    for (var i = 0; i < e; i++) {
+	var temp = second;
+	second = first + second;
+	first = temp;
+    }
+    return first;
+}
+
+// When the fibo button is push, add a new fibonacci term
+var fiboadd = function() {
+    var newFibo = document.createElement("li");
+    newFibo.className = "fol"
+    newFibo.innerHTML = fibo(fibonum.length);
+    fibolist.appendChild(newFibo);
+}
+
 for (var x = 0; x < list.length; x++) {
     list[x].addEventListener("mouseover", changeHeading);
     list[x].addEventListener("mouseout", revertHeading);
@@ -41,3 +64,4 @@ for (var x = 0; x < list.length; x++) {
 }
 
 btn.addEventListener("click", add);
+fibobtn.addEventListener("click", fiboadd);
